41fdb873dcc2bc6fa92fca7225579b53f4405583
Luke <lucas.parzych@thrivebio.com>
2018-09-01 08:35:31 -0400

    multiple concurrent calls
    also cleaned up code and fixed some phone numbers

--- bin/test.js
+++ bin/test.js
@@ -4,9 +4,25 @@ const fs = require('fs-extra');
 const { sid, authToken } = JSON.parse(fs.readFileSync('/run/secrets/twilio-credentials', 'utf8'));
 const client = new Twilio(sid, authToken);
 
-client.messages.create({
-    body: 'Nick this is a message from the future. You smell',
-    to: '+860808765',  // Text this number
-    from: '+3156276319' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+const contacts = [
+  { name: 'Brendan', number: '+18572312588' },
+  { name: 'Nick', number: '+18608087654' },
+  { name: 'Luke', number: '+13158865733' },
+]
+
+const twilioEnabledNumber = '+13156276319';
+
+main();
+async function main() {
+  try {
+    for (contact of contacts) {
+      const message = await client.messages.create({
+        body: `Helo ${contact.name}`,
+        to: contact.number,
+        from: twilioEnabledNumber
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
